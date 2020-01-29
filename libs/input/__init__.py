bcc187e40a689ca138aea3689e19514f7921f09c
OdZarref <ludwigvonmarcelo@gmail.com>
2020-01-29 20:55:01 -0300

    adicionando resposta para input de inválido de data de nascimento

--- libs/input/__init__.py
+++ libs/input/__init__.py
@@ -5,16 +5,20 @@ def read_name_surname():
 
 
 def read_birth_date():
+    from libs.interface import lines
+
     while True:
         birth_date = str(input('Birth Date: '))
 
         if len(birth_date) == 6 or len(birth_date) == 8 or len(birth_date) == 0:
             break
+        else:
+            lines('DEVEM SER VALORES NUMÉRICOS DE 6 OU 8 CARACTERES DE COMPRIMENTO.\nEX: 190201')
 
     return birth_date
 
 
 def read_nicknames():
-    nicknames = str(input('Apelido: ')).split()
+    nicknames = str(input('Nickname: ')).split()
 
     return nicknames
