da91fadbc8e229e3d08c2d527f291e84e717ea0f
OdZarref <ludwigvonmarcelo@gmail.com>
2020-02-03 23:21:55 -0300

    Adicionando novas possibilidades de escritura de senhas para apelidos caso haja a existência de um sobrenome cujo tamanho seja menor que 3 caracteres

--- kreator.py
+++ kreator.py
@@ -4,25 +4,24 @@ from interface import *
 from input import *
 
 mostra_logo()
-name = ''
+nome = ''
 sobrenomes = list()
-data_name = leia_nome_completo()
-data_nascimento = leia_nascimento()
+dados_nome = leia_nome_completo()
+dados_nascimento = leia_nascimento()
 apelidos = leia_apelidos()
 
-if len(data_name) == 0 and len(data_nascimento) == 0 and len(apelidos) == 0:
+if len(dados_nome) == 0 and len(dados_nascimento) == 0 and len(apelidos) == 0:
     linhas('No arguments have been entered. Program closed.')
 else:
-    complementos = criar_complementos(data_nascimento)
+    complementos = criar_complementos(dados_nascimento)
 
-    if len(data_name) != 0:
-        for names in data_name:
-            if names == data_name[0]:
-                name = names
+    if len(dados_nome) != 0:
+        for nomes in dados_nome:
+            if nomes == dados_nome[0]:
+                nome = nomes
+                nome_arquivo = nome
             else:
-                sobrenomes.append(names)
-
-        nome_arquivo = name
+                sobrenomes.append(nomes)
     else:
         nome_arquivo = 'passwords'
         
@@ -30,46 +29,70 @@ else:
     if not verificar_arquivo:
         criar_arquivo(nome_arquivo)
 
-    if len(data_nascimento) != 0:
-        escrever_senhas_nascimento(nome_arquivo, data_nascimento)
+    if len(dados_nascimento) != 0:
+        escrever_senhas_nascimento(nome_arquivo, dados_nascimento)
 
-    if name != '':
-        escrever_senhas(nome_arquivo, name, complementos)
+    if nome != '':
+        escrever_senhas(nome_arquivo, nome, complementos)
 
-    sobrenome_pequeno = concatenacao_sobrenome_pequeno(name, sobrenomes)
+    sobrenome_pequeno = sobrenome_pequeno(nome, sobrenomes)
     if sobrenome_pequeno != '':
-        escrever_senhas(nome_arquivo, sobrenome_pequeno, complementos)
+        escrever_senhas(nome_arquivo, nome + sobrenome_pequeno, complementos)
         
-    sobrenomes = remove_sobrenome_pequeno(name, sobrenomes)
+    sobrenomes = remove_sobrenome_pequeno(nome, sobrenomes)
 
     if len(sobrenomes) == 0:
-        escrever_senhas(nome_arquivo, name, complementos)
+        escrever_senhas(nome_arquivo, nome, complementos)
     elif len(sobrenomes) == 1:
-        escrever_um_nome(nome_arquivo, name, sobrenomes, complementos)
+        escrever_um_nome(nome_arquivo, nome, sobrenomes, complementos)
     elif len(sobrenomes) == 2:
-        escrever_dois_nomes(nome_arquivo, name, sobrenomes, complementos)
+        escrever_dois_nomes(nome_arquivo, nome, sobrenomes, complementos)
     elif len(sobrenomes) == 3:
-        escrever_tres_nomes(nome_arquivo, name, sobrenomes, complementos)
+        escrever_tres_nomes(nome_arquivo, nome, sobrenomes, complementos)
     elif len(sobrenomes) == 4:
-        escrever_quatro_nomes(nome_arquivo, name, sobrenomes, complementos)
+        escrever_quatro_nomes(nome_arquivo, nome, sobrenomes, complementos)
 
     if len(apelidos) > 0:
         for apelido in apelidos:
             if len(sobrenomes) == 0:
+                if sobrenome_pequeno != '':
+                    escrever_senhas(nome_arquivo, apelido + sobrenome_pequeno, complementos)
+
                 escrever_senhas(nome_arquivo, apelido, complementos)
+
             elif len(sobrenomes) == 1:
+                if sobrenome_pequeno != '':
+                    escrever_senhas(nome_arquivo, apelido + sobrenome_pequeno, complementos)
+
                 escrever_um_nome(nome_arquivo, apelido, sobrenomes, complementos)
+
             elif len(sobrenomes) == 2:
+                if sobrenome_pequeno != '':
+                    escrever_senhas(nome_arquivo, apelido + sobrenome_pequeno, complementos)
+
                 escrever_dois_nomes(nome_arquivo, apelido, sobrenomes, complementos)
+
             elif len(sobrenomes) == 3:
+                if sobrenome_pequeno != '':
+                    escrever_senhas(nome_arquivo, apelido + sobrenome_pequeno, complementos)
+
                 escrever_tres_nomes(nome_arquivo, apelido, sobrenomes, complementos)
             elif len(sobrenomes) == 4:
+                if sobrenome_pequeno != '':
+                    escrever_senhas(nome_arquivo, apelido + sobrenome_pequeno, complementos)
+                    
                 escrever_quatro_nomes(nome_arquivo, apelido, sobrenomes, complementos)
 
     for surname in sobrenomes:
+        if sobrenome_pequeno != '':
+            escrever_senhas(nome_arquivo, apelido + sobrenome_pequeno, complementos)
+
         escrever_senhas(nome_arquivo, surname, complementos)
 
     for apelido in apelidos:
+        if sobrenome_pequeno != '':
+            escrever_senhas(nome_arquivo, apelido + sobrenome_pequeno, complementos)
+
         escrever_senhas(nome_arquivo, apelido, complementos)
 
     linhas(f'Passwords written: {conta_senhas(nome_arquivo)}')
